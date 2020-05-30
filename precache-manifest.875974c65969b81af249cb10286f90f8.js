b07e0c423ed39ba4046887c19a5f7d9e7af1c6e1
Jason <jason.silvers@outlook.com>
2020-05-30 18:54:56 -0500

    Updates

---
+++ precache-manifest.875974c65969b81af249cb10286f90f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a392adbf6cd6ddc63ecb732f26c49f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94765c8697994ddac54",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7228ca6b0ebc256b3ce4",
+    "url": "/static/js/2.0fd704ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fd704ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94765c8697994ddac54",
+    "url": "/static/js/main.88fe8047.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/static/js/main.88fe8047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1763580a059ef1808171",
+    "url": "/static/js/runtime-main.bf23439e.js"
+  }
+]);
